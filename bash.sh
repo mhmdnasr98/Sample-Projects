2a5293e4ed51bbea9d1f8d2c87db8ed4d3bfafb1
mhmdnasr98 <167673412+mhmdnasr98@users.noreply.github.com>
2024-05-14 14:26:05 +0300

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This is a simple Bash test script
+
+echo "Hello, this is a test script."
+echo "Current directory: $(pwd)"
+echo "List of files in the current directory:"
+ls -l
+echo "Script executed successfully."
+echo "Goodbye"
